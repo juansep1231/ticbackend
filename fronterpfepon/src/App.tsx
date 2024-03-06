af92794abdc2a5c25530fc72784395cf492437cb
Valery Vallejo <valery.vallejo@epn.edu.ec>
2024-03-06 01:51:54 -0500

    fix: basic configurations

--- fronterpfepon/src/App.tsx
+++ fronterpfepon/src/App.tsx
@@ -1,56 +1,34 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Generic/Navbar.tsx";
+import LoginPage from "./landing/login/loginPage.tsx";
+import HomePage from "./landing/home/homePage.tsx";
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+  const [darkMode, setDarkMode] = useState(false);
+  const [language, setLanguage] = useState("en");
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+  const toggleLanguage = () => {
+    // Lógica para cambiar el idioma
+  };
+  return (
+    <Router>
+      <div className="h-screen">
+        <Navbar
+          toggleDarkMode={toggleDarkMode}
+          toggleLanguage={toggleLanguage}
+        />
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
