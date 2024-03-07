0779c8249834ccda53d3c65c89c494b994e03317
juanse <juan.posso@epn.edu.ec>
2024-03-07 02:54:43 -0500

    feat: Add InventoryTable component to InventoryHomePage

--- fronterpfepon/src/landing/inventory/inventoryHomePage.tsx
+++ fronterpfepon/src/landing/inventory/inventoryHomePage.tsx
@@ -1,12 +1,15 @@
 import NavbarLogin from "../../components/Generic/NavbarLogin";
-
+import InventoryTable from "./components/InventoryTable";
 const InventoryHomePage = () => {
   return (
-    <div className="dark:bg-neutral-950 w-full h-screen">
+    <div className="w-full h-screen">
       <NavbarLogin />
       <div className="flex flex-col mx-auto justify-center gap-10 md:max-w-7xl p-8">
         <div className="">
           <h1 className="text-5xl">Inventario</h1>
+        </div>
+        <div className="flex justify-center">
+          <InventoryTable />
         </div>
       </div>
     </div>
