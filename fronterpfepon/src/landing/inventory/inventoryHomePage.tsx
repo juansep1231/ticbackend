8611bca94a64ebcc5c057d0230f54a6897ce3088
juanse <juan.posso@epn.edu.ec>
2024-03-07 11:05:58 -0500

    feat: Add Download PDF button to inventory home page

--- fronterpfepon/src/landing/inventory/inventoryHomePage.tsx
+++ fronterpfepon/src/landing/inventory/inventoryHomePage.tsx
@@ -1,4 +1,5 @@
 import NavbarLogin from "../../components/Generic/NavbarLogin";
+import DownloadPDFButton from "./components/DownloadPDFButton";
 import InventoryTable from "./components/InventoryTable";
 const InventoryHomePage = () => {
   return (
@@ -7,6 +8,9 @@ const InventoryHomePage = () => {
       <div className="flex flex-col mx-auto justify-center gap-10 md:max-w-7xl p-8">
         <div className="">
           <h1 className="text-5xl">Inventario</h1>
+        </div>
+        <div className="flex justify-end">
+          <DownloadPDFButton />
         </div>
         <div className="flex justify-center">
           <InventoryTable />
